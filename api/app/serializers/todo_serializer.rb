49888e8a78aa31a36cf16427ec6ed1cf4cb7fc48
suzutuki <renath31@gmail.com>
2022-01-27 09:51:41 +0900

    シリアライザーの追加(Todo)

--- api/app/serializers/todo_serializer.rb
+++ api/app/serializers/todo_serializer.rb
@@ -1,3 +1,9 @@
-class TodoSerializer < ActiveModel::Serializer
-  attributes :id
-end
+class UserSerializer < ActiveModel::Serializer
+    attributes :id, :title, :user_id, :username
+    belongs_to :user
+  # usernameというメソッドを作り、名前の値をTodoと一緒に返せるように設定
+    def username
+      object.user.name
+    end
+  end
+  
