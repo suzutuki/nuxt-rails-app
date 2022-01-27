f274cb23035273d32c836ddb35cd94755ffaac05
suzutuki <renath31@gmail.com>
2022-01-27 09:44:15 +0900

    ユーザーモデルの追加

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_27_002831) do
+ActiveRecord::Schema.define(version: 2022_01_27_004347) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "uid"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
