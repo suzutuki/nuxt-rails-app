56a1afe0b168dc8fc9680c233f96b7a4c4a4bea0
suzutuki <renath31@gmail.com>
2022-01-27 10:25:30 +0900

    コメントを追加

---
+++ front/components/AddTodo.vue
@@ -0,0 +1,36 @@
+<template>
+<!-- <v-form>というタグで囲まれたTODO追加のフォームが、コンポーネントとして表示される -->
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="4">
+<!-- v-model="title"とすることで、<script>のdata()の中で宣言されているtitleとデータを紐づける -->
+          <v-text-field v-model="title" counter="10" label="todo" required></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4">
+<!-- @clickという記法を使って、このボタンをクリックしたときに、methodsの中で定義した関数を呼び出す -->
+          <v-btn @click="handleSubmit">作成</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: ""
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.title = "";
+    }
+  }
+};
+</script>
+
+<style>
+</style>
+
---
+++ front/components/TodoList.vue
@@ -0,0 +1,46 @@
+<template>
+<!-- <v-card>の中にTODOリストのタイトルやテーブルを表示 -->
+  <v-card>
+    <v-card-title>
+      Todo List
+      <v-spacer></v-spacer>
+      <!-- TODOリストの検索バーを作成。v-modelで渡している値は検索バーに入っている初期値 -->
+      <v-text-field v-model="search" label="Search" single-line hide-details></v-text-field>
+   <!-- テキストフィールドで入力した値が、searchという値に入り
+   それがそのまま<v-text-table>のsearchプロパティの値として利用される -->
+    </v-card-title>
+    <!-- :headersというプロパティにはvueのheadersという変数の値が当てられている -->
+    <v-data-table :headers="headers" :items="todos" :search="search"></v-data-table>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+// titleはheadersのvalueがtitleの列に、usernameはheadersのvalueがusernameの列へ表示される
+      todos: [
+        {
+          title: "test",
+          username: "太郎"
+        }
+      ],
+      search: "",
+      headers: [
+        {
+          text: "タイトル",
+          align: "left",
+          sortable: false,
+          value: "title"
+        },
+        { text: "ユーザー名", 
+          value: "username"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
