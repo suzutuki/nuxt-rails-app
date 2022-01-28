c6bc52b1712d718cba5d03e55438e2beb658b613
suzutuki <renath31@gmail.com>
2022-01-28 22:29:42 +0900

    addTodoを修正

--- front/components/AddTodo.vue
+++ front/components/AddTodo.vue
@@ -1,15 +1,20 @@
 <template>
-<!-- <v-form>というタグで囲まれたTODO追加のフォームが、コンポーネントとして表示される -->
+  <!-- <v-form>というタグで囲まれたTODO追加のフォームが、コンポーネントとして表示される -->
   <v-form>
     <v-container>
       <v-row>
         <v-col cols="12" md="4">
-<!-- v-model="title"とすることで、<script>のdata()の中で宣言されているtitleとデータを紐づける -->
-          <v-text-field v-model="title" counter="10" label="todo" required></v-text-field>
+          <!-- v-model="title"とすることで、<script>のdata()の中で宣言されているtitleとデータを紐づける -->
+          <v-text-field
+            v-model="title"
+            counter="10"
+            label="todo"
+            required
+          ></v-text-field>
         </v-col>
         <v-col cols="12" md="4">
-<!-- @clickという記法を使って、このボタンをクリックしたときに、methodsの中で定義した関数を呼び出す -->
-<!-- クリックをすると、methodsに登録しているhandleSubmitと言う関数を実行 -->
+          <!-- @clickという記法を使って、このボタンをクリックしたときに、methodsの中で定義した関数を呼び出す -->
+          <!-- クリックをすると、methodsに登録しているhandleSubmitと言う関数を実行 -->
           <v-btn @click="handleSubmit">作成</v-btn>
         </v-col>
       </v-row>
@@ -21,15 +26,27 @@
 export default {
   data() {
     return {
-      title: ""
+      title: "",
     };
+  },
+
+  computed: {
+    user() {
+      return this.$store.state.auth.currentUser;
+    },
   },
   methods: {
     handleSubmit() {
-      this.$emit("submit", this.title);
+      // user_idも入れたtodoオブジェクトを送る。
+      const todo = {
+        title: this.title,
+        user_id: this.user.id,
+      };
+
+      this.$emit("submit", todo);
       this.title = "";
-    }
-  }
+    },
+  },
 };
 </script>
 
