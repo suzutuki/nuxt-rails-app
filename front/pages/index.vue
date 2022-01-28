cb9e01a5b598423c992c26d55e754db639e876a1
suzutuki <renath31@gmail.com>
2022-01-28 22:31:24 +0900

    indexを修正

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,48 +1,48 @@
 <template>
   <div v-if="user">
-    <p>名前：{{user.name}}</p>
+    <p>名前：{{ user.name }}</p>
     <AddTodo @submit="addTodo" />
-    <TodoList :todos="todos" />
+    <TodoList :todos="user.todos" />
   </div>
 </template>
 
 <script>
 // <script>の中でimportし、importしたコンポーネントをexportの中でcomponentsに登録
-  import AddTodo from "@/components/AddTodo";
+import AddTodo from "@/components/AddTodo";
 // @はv-on:@submit="addTodo"の省略形
-  import TodoList from "@/components/TodoList";
+import TodoList from "@/components/TodoList";
 // axiosのインポート
-  import axios from "@/plugins/axios";
-  export default {
-    components: {
-      AddTodo,
-      TodoList,
+import axios from "@/plugins/axios";
+export default {
+  components: {
+    AddTodo,
+    TodoList,
+  },
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  // 正しい場所にuserと書くと、computedプロパティが動き、
+  // storeの中のauth.jsのstateのcurrentUserという値が返される。
+  computed: {
+    user() {
+      return this.$store.state.auth.currentUser;
     },
-    data() {
-      return {
-        todos: [],
-      };
+  },
+  created() {
+    console.log("API_KEY:", process.env.API_KEY);
+  },
+  methods: {
+    async addTodo(title) {
+      const { data } = await axios.post("/v1/todos", { todo });
+      this.$store.dispatch("auth/setUser", {
+        ...this.user,
+        todos: [...this.user.todos, data],
+      });
     },
-    // 正しい場所にuserと書くと、computedプロパティが動き、
-    // storeの中のauth.jsのstateのcurrentUserという値が返される。
-    computed: {
-      user() {
-        return this.$store.state.auth.currentUser;
-      }
-    },
-     created() {
-      console.log("API_KEY:", process.env.API_KEY);
-    },
-    methods: {
-      async addTodo(title) {
-        await axios.post("/v1/todos", { title });
-        this.todos.push({
-          title
-        });
-      },
-    },
-  };
-
+  },
+};
 </script>
 
 <style>
