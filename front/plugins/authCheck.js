e5c866031d6723dc1b35b980e660007996a1c6c8
suzutuki <renath31@gmail.com>
2022-01-28 15:35:40 +0900

    コメントの追加

--- front/plugins/authCheck.js
+++ front/plugins/authCheck.js
@@ -2,6 +2,7 @@ import firebase from "@/plugins/firebase";
 import axios from "@/plugins/axios";
 
 const authCheck = ({ store, redirect }) => {
+    // onAuthStateChanged ログイン状態のユーザーの情報をFirebaseへ確認
   firebase.auth().onAuthStateChanged(async user => {
     if (user) {
       const { data } = await axios.get('/v1/users', {
@@ -10,6 +11,7 @@ const authCheck = ({ store, redirect }) => {
         },
       });
       console.log("ログインしているユーザー:", data);
+    //   auth.jsのsetUserアクションに適用
       store.dispatch("auth/setUser", data)
     } else {
       store.dispatch("auth/setUser", null)
--- front/store/auth.js
+++ front/store/auth.js
@@ -1,14 +1,19 @@
 export const state = () => ({
+  // currentUserという値として、ユーザーの情報の入れ物を作成
     currentUser: null
   });
   
   export const mutations = {
+    // stateに書き込むメソッドを作成
     setUser(state, payload) {
+    // 第二引数として渡ってきた情報をcurrentUserに入れている
       state.currentUser = payload
     },
   };
   
   export const actions = {
+  // コンテキストオブジェクトのStoreインスタンスを受け取って、
+  // setUserという名前でmutationsを呼び出す。
     setUser (context, data) {
       context.commit('setUser', data)
     }
