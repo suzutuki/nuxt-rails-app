c6aab5d08d67148967dbe6c7f50543e85f65890e
suzutuki <renath31@gmail.com>
2022-01-28 22:38:01 +0900

    Todosコントローラーの修正

--- api/app/controllers/v1/todos_controller.rb
+++ api/app/controllers/v1/todos_controller.rb
@@ -6,8 +6,14 @@ class V1::TodosController < ApplicationController
         end
     
         def create
-          puts params
-        end
+            todo = Todo.new(todo_params)
+            if todo.save
+              render json: todo
+            else
+              render json: todo.errors
+            end
+            puts params
+          end
     
         def destroy
           todo = Todo.find(params[:id])
--- front/pages/index.vue
+++ front/pages/index.vue
@@ -2,6 +2,7 @@
   <div v-if="user">
     <p>名前：{{ user.name }}</p>
     <AddTodo @submit="addTodo" />
+    <!-- :todosの値は、Vuexで管理しているcurrentUserのtodoプロパティの値を送る。 -->
     <TodoList :todos="user.todos" />
   </div>
 </template>
@@ -34,7 +35,7 @@ export default {
     console.log("API_KEY:", process.env.API_KEY);
   },
   methods: {
-    async addTodo(title) {
+    async addTodo(todo) {
       const { data } = await axios.post("/v1/todos", { todo });
       this.$store.dispatch("auth/setUser", {
         ...this.user,
