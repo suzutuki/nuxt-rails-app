e080552cbb23f7259f07fca63b0518a581b0edb2
suzutuki <renath31@gmail.com>
2022-01-29 14:11:54 +0900

    フラッシュメッセージの実装

--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -29,36 +29,20 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
+    <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
+        <v-icon>mdi-{{ `chevron-${miniVariant ? "right" : "left"}` }}</v-icon>
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
+      <v-btn icon @click.stop="clipped = !clipped">
         <v-icon>mdi-application</v-icon>
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
+      <v-btn icon @click.stop="fixed = !fixed">
         <v-icon>mdi-minus</v-icon>
       </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
+      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
         <v-icon>mdi-menu</v-icon>
       </v-btn>
     </v-app-bar>
@@ -67,84 +51,76 @@
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
+    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
       <v-list>
         <v-list-item @click.native="right = !right">
           <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
+            <v-icon light> mdi-repeat </v-icon>
           </v-list-item-action>
           <v-list-item-title>Switch drawer (click me)</v-list-item-title>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
+    <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
+import Loading from "@/components/Loading";
+import Success from "@/components/Success"; 
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: "TODO App",
+    };
+  },
 
- import Loading from "@/components/Loading"; 
+  components: {
+    Loading,
+    Success
+  },
 
-  export default {
-    data() {
-      return {
-        clipped: false,
-        drawer: false,
-        fixed: false,
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'TODO App'
+  computed: {
+    user() {
+      return this.$store.state.auth.currentUser;
+    },
+    items() {
+      if (this.user) {
+        return [
+          {
+            icon: "mdi-apps",
+            title: "TODOS",
+            to: "/",
+          },
+          {
+            icon: "mdi-chart-bubble",
+            title: "MYPAGE",
+            to: "/mypage",
+          },
+        ];
+      } else {
+        return [
+          {
+            icon: "mdi-apps",
+            title: "LOGIN",
+            to: "/login",
+          },
+          {
+            icon: "mdi-chart-bubble",
+            title: "SIGNUP",
+            to: "/signup",
+          },
+        ];
       }
     },
-
-    components: {
-      Loading
-    },
-    
-    computed: {
-      user() {
-        return this.$store.state.auth.currentUser;
-      },
-      items() {
-        if (this.user) {
-          return [{
-              icon: "mdi-apps",
-              title: "TODOS",
-              to: "/"
-            },
-            {
-              icon: "mdi-chart-bubble",
-              title: "MYPAGE",
-              to: "/mypage"
-            }
-          ];
-        } else {
-          return [{
-              icon: "mdi-apps",
-              title: "LOGIN",
-              to: "/login"
-            },
-            {
-              icon: "mdi-chart-bubble",
-              title: "SIGNUP",
-              to: "/signup"
-            }
-          ];
-        }
-      }
-    }
-  }
+  },
+};
 </script>
